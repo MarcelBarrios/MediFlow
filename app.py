cfefb65c326455aa774f5c49f3cbe9b835084e97
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-29 15:45:04 -0400

    routed new patient to all patients

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_pymongo import PyMongo
@@ -20,9 +19,12 @@ app.secret_key = os.getenv("SECRET_KEY")
 app.config["MONGO_URI"] = os.getenv(
     "MONGO_URI", "mongodb://localhost:27017/mediflow")
 mongo = PyMongo(app)
+app.mongo = mongo
 
 # Collections
 # mediflow_collection = mongo.db.mediflow
+new_patients_collection = mongo.db.patients
+
 
 
 # --- Import and Register Blueprints ---
